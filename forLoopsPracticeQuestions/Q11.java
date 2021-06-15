ab881014b336d89912b87bb0401c8638de3fc225
Aditya Raul <78086929+RaulLit@users.noreply.github.com>
2021-06-15 16:16:39 +0530

    Add files via upload

---
+++ forLoopsPracticeQuestions/Q11.java
@@ -0,0 +1,26 @@
+package forLoopsPracticeQuestions;
+import java.util.*;
+
+public class Q11 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		//THIS QUESTION IS CALLED PASCAL'S TRIANGLE
+		for(int i = 0; i <= n; i++) {
+			int number = 1;
+			for(int j = i; j<n; j++) {
+				System.out.print("  ");
+			}
+			
+			for (int j = 0; j <= i; j++) {
+				System.out.print(number + "   ");
+                number = number * (i - j) / (j + 1);	//This is what makes it work
+            }
+			
+			System.out.println();
+		}
+	}
+
+}
