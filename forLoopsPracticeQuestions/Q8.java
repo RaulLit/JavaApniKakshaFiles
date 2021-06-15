6f6e44c0d16ce27c068499e61d9912bdfc0c73e7
Aditya Raul <78086929+RaulLit@users.noreply.github.com>
2021-06-15 15:32:47 +0530

    Update Q8.java

--- forLoopsPracticeQuestions/Q8.java
+++ forLoopsPracticeQuestions/Q8.java
@@ -8,19 +8,25 @@ public class Q8 {
 		
 		int n = sc.nextInt();
 		
-		for (int i =1; i <= n; i++) {
+		for (int i=1; i <= n+1; i++) {
+			//Right side till column 0
 			for (int j =1; j<= n-i+1; j++) {
 				System.out.print("  ");
 			}
-			
-			for (int j =1 ; j <= n; j++) {
-				System.out.print(j);
+			if(i>1) {
+				for (int j = n-i+2; j <= n; j++) {
+					System.out.print(j + " ");
+				}
+			}
+			System.out.print(0 + " ");
+			//Left side after column 0
+			if(i>1) {
+				for (int j = n; j >= n-i+2; j--) {
+					System.out.print(j + " ");
+				}
 			}
 			System.out.println();
 		}
 		
-		
-
 	}
-
 }
